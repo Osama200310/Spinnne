972d01092e604ce508a537c32e83e7ac12a39a2c
Tammo Lütten <147433499+MrBowlin@users.noreply.github.com>
2025-11-09 11:22:07 +0100

    Add files via upload

--- walkie_main.ino
+++ walkie_main.ino
@@ -22,14 +22,18 @@ Author      : UlliS
 #define IR_PIN A5
 #define MODEL 1080
 
-const int STEER_CORRECTION = 117;
+const int STEER_CORRECTION = 114;
 const int STEERING_FACTOR = 100;
 const int MIN_THRESHHOLD = 40;
-const int MAX_THRESHHOLD = 60;
+const int MAX_THRESHHOLD = 80;
 const int CRIT_THRESHHOLD = 20;
 const int TURNING_RATE = 1000;
 const int MOVING_RATE = 1000;
-const int BACKOFF_RATE = 2000;
+const int BACKOFF_RATE = 1500;
+const int DANCE_FACTOR = 80;
+const int DANCE_DELAY = 500;
+const int MVMNT_BUFFER = 500;
+const int DIST_OFFSET = 8;
 
 int distance_cm = 0;
 
@@ -62,6 +66,7 @@ void setup()
     // set the data rate for the SoftwareSerial port (User-Board to Locomotion-Controller)
     SERIAL_CMD.begin(SERIAL_CMD_BAUD);
     
+    
     // reset the Locomotion-Controller
     ROBOT_RESET();
     delay(250);
@@ -85,7 +90,6 @@ void loop()
     // main loop
     while(1)
     {
-        
         int _hight = 60;    // init robot hight
         
         if(!digitalRead(T1)) 
@@ -93,6 +97,7 @@ void loop()
             delay(50);
             if(!digitalRead(T1))
             {   
+                MSound(1, 100, 1000);
                 ROBOT_INIT();                   // reset etc.
                 ROBOT_PWR_ON();                 // power on
                 ROBOT_HEIGHT(_hight);           // init hight
@@ -100,56 +105,100 @@ void loop()
                 ROBOT_GAINT_MODE(WAVE_24);
                 delay(500);
 
+                bool is_dancing = true;
+                ROBOT_ROTATE_MODE(128,128,128,128);
+                delay(DANCE_DELAY);
+
+                // dancing loop
+                while(is_dancing == true) {
+
+                    ROBOT_ROTATE_MODE(128 + DANCE_FACTOR,128,128,128 - DANCE_FACTOR / 2);
+                    delay(DANCE_DELAY);
+
+                    ROBOT_ROTATE_MODE(128,128,128,128 + DANCE_FACTOR / 2);
+                    delay(DANCE_DELAY);
+
+                    ROBOT_ROTATE_MODE(128 - DANCE_FACTOR,128,128,128 - DANCE_FACTOR / 2);
+                    delay(DANCE_DELAY);
+
+                    ROBOT_ROTATE_MODE(128,128,128,128 + DANCE_FACTOR / 2);
+                    delay(DANCE_DELAY);
+
+                    if (!digitalRead(T2)) {
+                        delay(50);
+                        if(!digitalRead(T2)) {
+                            is_dancing = false;
+                        }
+                    }
+                }
+
                 // steering loop / steering mode
                 bool is_close = false;
                 double detected_dist = 0;
                 bool change_dir = false;
                 bool is_running = true;
+                bool backoff = false;
 
-                while(is_running) {
+                while (is_running == true) {
                     distance_cm = mySensor.distance();
 
-                    if (distance_cm < MIN_THRESHHOLD) {
-                        if (distance_cm < CRIT_THRESHHOLD) {
-                            ROBOT_MOVE(128, STEER_CORRECTION, 255);
-                            delay(BACKOFF_RATE);
-                        }                
-                        if (is_close == false) {
-                            detected_dist = distance_cm;             
-                            is_close = true;
-                        }   
-                    } else if (is_close == true && distance_cm > MAX_THRESHHOLD){
+                    if (distance_cm < MIN_THRESHHOLD && is_close == false) {
+                        detected_dist = distance_cm;
+                        is_close = true;   
+                    } else if (distance_cm > MAX_THRESHHOLD) {
                         is_close = false;
                         change_dir = false;
+                        backoff = false;
                     }
 
                     if (is_close == true) {
-                        if (!change_dir == false || distance_cm < detected_dist) {
+                        if (distance_cm < (detected_dist - DIST_OFFSET) && change_dir == false) {
                             change_dir = true;
-                            ROBOT_MOVE(128, STEER_CORRECTION - STEERING_FACTOR, 128);
-                            delay(TURNING_RATE);
-                        } else {
-                            ROBOT_MOVE(128, STEER_CORRECTION + STEERING_FACTOR, 128);
-                            delay(TURNING_RATE);
                         }
-                    } else {
-                        ROBOT_MOVE(128, STEER_CORRECTION, 0);
-                        delay(MOVING_RATE);
+                        else if (distance_cm < CRIT_THRESHHOLD && backoff == false) {
+                            backoff = true;
+                        }
                     }
 
-                    if (!digitalRead(T2)) {
-                        delay(50);
-                        if(!digitalRead(T2)) {
+
+
+                    if (is_close == true) {
+                        if (change_dir == false)
+                            ROBOT_MOVE(128, STEER_CORRECTION + STEERING_FACTOR, 128);
+                        else
+                            ROBOT_MOVE(128, STEER_CORRECTION - STEERING_FACTOR, 128);
+                        } 
+                    else if (backoff == true)
+                        ROBOT_MOVE(128, STEER_CORRECTION, 255);
+                    else
+                        ROBOT_MOVE(128, STEER_CORRECTION, 0);
+
+                    delay(MVMNT_BUFFER);
+
+                    
+                    if (Serial.available() > 0) {
+                        int input = Serial.read() - '0';
+                        if (input == 1) {
                             is_running = false;
                         }
                     }
+                    
                 }
+
                 
                 ROBOT_MOVE(128,128,128);        // stop
                 delay(500);
                 
                 ROBOT_HEIGHT(0);                // sit down
-                delay(1000);                
+                delay(1000);  
+
+                for (int i = 0; i < 10; i++)      // end beeping
+                {
+                    MSound(3, 100, 1000, 50, 2000, 100, 3500);
+                    delay(500);
+                }
+
+                delay(5000);
                 
                 ROBOT_PWR_OFF();                // power off
                 delay(1500);                
